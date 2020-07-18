27a740a47ec7c211c55a625fa4c5602eca805f0e
Nuno Luciano <1905497+gigamaster@users.noreply.github.com>
2020-07-18 06:46:38 +0200

    Pr/224 (#228)
    * Import Smarty 3.1.35
    
    * modified for Smarty 3.1
    
    * fix more for smarty3
    
    * forget import file from smarty3
    
    Co-authored-by: Nobuhiro YASUTOMI <nobuhiro.yasutomi@nifty.ne.jp>
    Co-authored-by: nunoluciano <nunoluciano@yahoo.com>

--- xoops_trust_path/libs/smarty/plugins/modifiercompiler.noprint.php
+++ xoops_trust_path/libs/smarty/plugins/modifiercompiler.noprint.php
@@ -1,1 +1,20 @@
-../../smarty3/libs/plugins/modifiercompiler.noprint.php
+<?php
+/**
+ * Smarty plugin
+ *
+ * @package    Smarty
+ * @subpackage PluginsModifierCompiler
+ */
+/**
+ * Smarty noprint modifier plugin
+ * Type:     modifier
+ * Name:     noprint
+ * Purpose:  return an empty string
+ *
+ * @author Uwe Tews
+ * @return string with compiled code
+ */
+function smarty_modifiercompiler_noprint()
+{
+    return "''";
+}
